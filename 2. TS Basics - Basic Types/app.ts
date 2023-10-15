99c28d04092fe412c1fc01699ef6386f833e8a6f
ArditZubaku <zubaku92@gmail.com>
2023-10-16 01:13:15 +0200

    Section 2: Enums.

--- 2. TS Basics - Basic Types/app.ts
+++ 2. TS Basics - Basic Types/app.ts
@@ -2,7 +2,7 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
+/* const person: {
   name: string;
   age: number;
   hobbies: string[];
@@ -13,6 +13,38 @@ const person: {
   age: 30,
   hobbies: ["Sports", "Cooking"],
   role: [2, "author"],
+}; */
+
+// const ADMIN = 0;
+// const STUDENT = 1;
+// const TEACHER = 2;
+
+enum Role {
+  // By default they are assigned a value of number starting from 0
+  ADMIN, //same as ADMIN = 0
+  STUDENT, // STUDENT = 1
+  TEACHER, // TEACHER = 2
+}
+
+enum Role2 {
+  // If we assign the first value, then it will increment from there
+  ADMIN = 10,
+  STUDENT, // same as STUDENT = 11
+  TEACHER, // same as TEACHER = 12
+}
+
+enum Role3 {
+  // We can assign different values to each one
+  ADMIN = 101,
+  STUDENT = "Student",
+  TEACHER = 202,
+}
+
+const person = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
 
 // Pushing works tho, even tho it should'nt!!!
