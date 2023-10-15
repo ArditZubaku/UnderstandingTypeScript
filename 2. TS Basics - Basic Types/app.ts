11e7cda00bbe23c14f8c6f3879485ae089ae2f6c
ArditZubaku <zubaku92@gmail.com>
2023-10-16 01:01:58 +0200

    Section 2: Basic types (number, string, boolean, object, array, tuple).

---
+++ 2. TS Basics - Basic Types/app.ts
@@ -0,0 +1,32 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  // Tuple type -> special type of array specified to have only 2 elements, where 1st element should be of type number, 2nd of type string
+  role: [number, string];
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+// Pushing works tho, even tho it should'nt!!!
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
