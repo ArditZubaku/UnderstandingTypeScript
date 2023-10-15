8e6cca256893a042bbeeb6cfe996e14eccae7519
ArditZubaku <zubaku92@gmail.com>
2023-10-16 01:39:34 +0200

    Section 2: Literal types.

--- 2. TS Basics - Basic Types/app.ts
+++ 2. TS Basics - Basic Types/app.ts
@@ -1,15 +1,32 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  //   resultForm: string
+  // Literal types -> value defined types
+  resultForm: "as-number" | "as-string"
+) {
   let result: string | number;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultForm === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+
+  /*   if (resultForm === "as-number") {
+    return +result;
+  } else {
+    return result;
+  } */
 }
 
-const combinedAges = combine(30, 50);
+const combinedAges = combine(30, 50, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Test", "1");
+const combinedNames = combine("Test", "1", "as-string");
 console.log(combinedNames);
+
+const combinedStringAges = combine("30", "50", "as-number");
+console.log(combinedStringAges);
