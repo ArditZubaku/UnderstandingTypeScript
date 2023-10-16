0df0eccf8d6bb3c7b68e51d05e56d52c5df6f6a7
ArditZubaku <zubaku92@gmail.com>
2023-10-16 23:21:31 +0200

    Section 2: Functions as types.

--- 2. TS Basics - Basic Types/app.ts
+++ 2. TS Basics - Basic Types/app.ts
@@ -10,3 +10,13 @@ function returnUndefined(n: number): undefined {
   console.log(n);
   return;
 }
+
+// let combinedValues: Function;
+// CombinedValues is a type of a function that accepts 2 numbers and returns a number
+let combinedValues: (n1: number, n2: number) => number;
+
+combinedValues = add;
+// combinedValues = 5;
+// combinedValues = printNumbers;
+
+console.log(combinedValues(8, 8));
