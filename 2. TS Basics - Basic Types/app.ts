288491ae607362c5185490ec1c30905e7c9d5842
ArditZubaku <zubaku92@gmail.com>
2023-10-16 23:06:35 +0200

    Section 2: Type aliases.

--- 2. TS Basics - Basic Types/app.ts
+++ 2. TS Basics - Basic Types/app.ts
@@ -1,9 +1,13 @@
+type NumberOrString = number | string;
+type AsStringOrAsNumber = "as-number" | "as-string";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
+  input1: NumberOrString,
+  input2: NumberOrString,
   //   resultForm: string
   // Literal types -> value defined types
-  resultForm: "as-number" | "as-string"
+  // resultForm: "as-number" | "as-string"
+  resultForm: AsStringOrAsNumber
 ) {
   let result: string | number;
   if (
@@ -30,3 +34,13 @@ console.log(combinedNames);
 
 const combinedStringAges = combine("30", "50", "as-number");
 console.log(combinedStringAges);
+
+type User = { name: string; age: number };
+
+function greet(user: User) {
+  console.log("Hi, I am " + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
