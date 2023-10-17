da5473003163797c8b45372f8ec7e6228afbe8c4
ArditZubaku <zubaku92@gmail.com>
2023-10-17 23:54:45 +0200

    Section 3: Setting a compilation target.

---
+++ 3. The TypeScript Compiler and its Configuration/analytics.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Run tsc --init to create tsconfig.json to compile the whole project at once");
+console.log("You can now run tsc --watch");
--- 3. The TypeScript Compiler and its Configuration/analytics.ts
+++ 3. The TypeScript Compiler and its Configuration/analytics.ts
@@ -3,3 +3,22 @@ console.log(
 );
 
 console.log("You can now run tsc --watch");
+
+const map = new Map();
+
+// testing();
+// test3();
+
+const test3 = function testing() {
+  console.log("Test");
+};
+
+test3();
+
+// testing2();
+
+const testing2 = () => {
+  console.log("Test 2");
+};
+
+testing2();
