fec76f1d681cbb6feeb1409cdf43713c3273579b
ArditZubaku <zubaku92@gmail.com>
2023-10-20 23:24:12 +0200

    Section 5: 'this' keyword.

--- 5. Classes & Interfaces/src/app.ts
+++ 5. Classes & Interfaces/src/app.ts
@@ -4,8 +4,25 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  // Ensures that only a type like this class can call this method
+  // Gotta be called 'this', and it is not considered a parameter
+  describe(this: Department) {
+    // 'this' represents whoever calls the method
+    console.log("Department" + this.name);
+  }
 }
 
-const department = new Department('Test');
+const department = new Department("Test");
 console.log(department);
 
+// const departmentCopy = {
+//   describe: department.describe,
+// };
+const departmentCopy = {
+  name: "test",
+  describe: department.describe,
+};
+
+// Undefined -> bc it has no name property
+departmentCopy.describe();
