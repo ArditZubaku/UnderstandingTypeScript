16bd1b19c3ad9f4428ff532c1f54be395af527bc
ArditZubaku <zubaku92@gmail.com>
2023-10-26 19:44:57 +0200

    Section 5: Interfaces.

---
+++ 5. Classes & Interfaces/src/interfaces.ts
@@ -0,0 +1,42 @@
+interface Person {
+  // You can not initialize an interface field since they all are abstract by default
+  name: string;
+  age: number;
+
+  // See, you can only declare the method structure, you can not implement it
+  greet(phrase: string): void;
+}
+
+// An interface can be used to declare object types
+let obj: Person;
+obj = {
+  age: 5,
+  name: "Test",
+  //   greet() {
+  //     console.log(this.name);
+  //   },
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
+  },
+};
+
+obj.greet("Hi there");
+
+interface Greetable {
+  name: string;
+  greet(phrase: string): void;
+}
+
+class Human implements Greetable {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
+  }
+}
+const person: Greetable = new Human("Test person");
+person.greet("Hello");
