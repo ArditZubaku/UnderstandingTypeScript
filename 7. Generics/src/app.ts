677e69e26cb5e40cd3beb68c37412d098484228d
Ardit Zubaku <zubaku92@gmail.com>
2023-12-02 19:22:36 +0100

    Section 7: Another generic function.

--- 7. Generics/src/app.ts
+++ 7. Generics/src/app.ts
@@ -44,3 +44,21 @@ const mergedObj2 = merge<
 function merge2<T extends object, K extends object>(objA: T, objB: K) {
   return Object.assign({}, objA, objB);
 }
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = "Got no description";
+  if (element.length === 1) {
+    description = "Got 1 element";
+  } else if (element.length > 1) {
+    description = "Got " + element.length + " elements";
+  }
+  return [element, description];
+}
+
+console.log(countAndDescribe("Test"));
+console.log(countAndDescribe([]));
+// console.log(countAndDescribe(1));
