3a26bd5cc637738116467f1c87a11c26e4d35b0b
Ardit Zubaku <zubaku92@gmail.com>
2023-12-02 20:48:33 +0100

    Section 7: Generic classes.

--- 7. Generics/src/app.ts
+++ 7. Generics/src/app.ts
@@ -69,3 +69,53 @@ function extractAndConvert<T extends object, K extends keyof T>(
 ): T[K] {
   return obj[key];
 }
+
+type PrimitiveType = number | string | boolean;
+
+class StorageClass<T extends PrimitiveType> {
+  private data: T[] = [];
+
+  public addItem(item: T): void {
+    this.data.push(item);
+  }
+
+  public removeItem(item: T): void {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1 if doesn't find the item's index => splice(-1) === from the end
+  }
+
+  public getItems(): T[] {
+    return [...this.data];
+  }
+}
+
+const storage = new StorageClass<string>();
+storage.addItem("new Item");
+// storage.addItem(10);
+const items = storage.getItems();
+
+const numbersStorage = new StorageClass<number>();
+numbersStorage.addItem(10);
+// numbersStorage.addItem("new Number");
+
+// const objectsStorage = new StorageClass<object>();
+// const testObj = { name: "Test" };
+// const testObj2 = { name: "Test2" };
+// objectsStorage.addItem({ name: "Test" });
+// objectsStorage.addItem({ name: "Test2" });
+// objectsStorage.addItem(testObj);
+// objectsStorage.addItem(testObj2);
+//...
+// Now we can correctly remove it since we have the same references now
+// objectsStorage.removeItem({ name: "Test" });
+// console.log(objectsStorage.getItems());
+
+// const testStorage = new StorageClass<"Test">();
+// testStorage.addItem('test');
+
+const booleanStorage = new StorageClass<boolean>();
+booleanStorage.addItem(true);
+booleanStorage.addItem(false);
+console.log(booleanStorage.getItems());
