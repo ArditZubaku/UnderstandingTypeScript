02deabf388671b2f64a41a0fe944df6e92d3fb74
Ardit Zubaku <zubaku92@gmail.com>
2023-12-03 03:09:37 +0100

    Section 8: Adding multiple decorators.

--- 8. Decorators/src/app.ts
+++ 8. Decorators/src/app.ts
@@ -8,6 +8,10 @@
 
 // When used like this, it should be called with arguments
 function Logger(logString: string) {
+  console.log(
+    "Logger Factory runs before any decorator function, 1 by 1, up -> down"
+  );
+
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -15,8 +19,13 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookID: string) {
+  // The factory runs before the decorator
+  console.log("Template Factory");
+
   // The actual decorator
   return function (constructor: any) {
+    console.log("Rendering template " + template);
+
     const hookElement = document.getElementById(hookID);
 
     const person: Person = new constructor();
@@ -28,8 +37,13 @@ function WithTemplate(template: string, hookID: string) {
   };
 }
 
+function RunsLast(_: Function) {
+  console.log("Decorators run bottom -> up, so this runs last.");
+}
+
+@RunsLast
 @WithTemplate("<h1>My person OBJ</h1>", "app")
-// @Logger("LOGGING - PERSON")
+@Logger("LOGGING - PERSON")
 class Person {
   name = "John";
 
